f80c6e9454f976e86d2ccdebab1bf28b09b60844
Nitin Solanki <nitinmlvya@gmail.com>
2019-12-21 05:28:31 +0530

    User details

--- flask_ecommerce/users/views.py
+++ flask_ecommerce/users/views.py
@@ -1,12 +1,26 @@
+import json
+
 from flask import Blueprint, jsonify
 
 mod = Blueprint('users', __name__, url_prefix='/users')
 
+data = json.load(open('data.json'))
+
 @mod.route('/', methods=['GET'])
 def fetchall():
-    return 'List of users.'
+    """
+    Fetch all the users
+    :return: List of the users.
+    """
+    return jsonify(data)
 
 @mod.route('/<user_id>', methods=['GET'])
-def show(user_id):
-    print(user_id)
-    return jsonify({})
+def fetch_one(user_id):
+    """
+    Fetch single user details
+    :param user_id: User ID
+    :return: Detail of the user
+    """
+    user_detail = [x for x in data if x['id'] == int(user_id)]
+    user_detail = user_detail[0] if user_detail else {}
+    return jsonify(user_detail)
